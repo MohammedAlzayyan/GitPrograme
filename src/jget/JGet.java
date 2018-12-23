0870a9b5c97255141f8fa59af1fb92629cfaecd1
= <=>
2018-12-23 11:41:07 +0200

    Sum

--- src/jget/JGet.java
+++ src/jget/JGet.java
@@ -15,7 +15,9 @@ public class JGet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("welcome");
+        int a = 5;
+        int b = 6;
+        System.out.println(a+b);
     }
     
 }
